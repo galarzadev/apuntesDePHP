e03466e264b75704fe381fb332e7e9193645bb06
Sergio Galarza <sergiogalarzalopez@gmail.com>
2021-04-12 20:07:22 -0500

    commit realizado desde el vs code para probar

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    jajajaja \n <br/>
+</body>
+</html>
 
+<?php
+require __DIR__ . '/vendor/autoload.php';
+//6
 class Answer
 {
     protected $clients = []; //comentarios
@@ -9,3 +23,78 @@ class Answer
     mas comentarios - (puntito de prueba)
     */
 }
+//7
+//Asignacion
+$num = 9;
+$lang = [
+    'es' => 'español',
+    'en' => 'inglés'
+];
+
+echo 'Suma 2+2: ' . ((int) 2 + (int) 2) . '\n\n';
+echo '\nResta 2-2: ' . ((int) 2 - (int) 2);
+echo 'Multiplica 2*2: ' . ((int) 2 * (int) 2);
+echo 'Divide 2/2: ' . ((int) 2 / (int) 2);
+echo 'Modulo 2%2: ' . ((int) 2 % (int) 2);
+echo 'werf';
+echo 'Exponencial 2**2: ' . ((int) 2 ** (int) 2);
+//8
+//comparacion
+//igual == y ===
+//Diferencias !=, !==
+//<, >, <=, >=
+
+//variables
+$app = 'name';
+$name ='platzi';
+
+echo $app; //name
+echo $$app; //platzi
+
+$a = (int) 2;
+
+if ($a==1) {
+    echo 'if 1';
+}
+elseif ($a==2) {
+    echo 'if 2 \n';
+}
+else {
+    echo 'if 3';
+}
+
+switch ($a) {
+    case '1':
+        echo 'switch 1';
+        break;
+    case '2':
+        echo 'switch 2';
+        break;
+    
+    default:
+        echo 'switch 3';
+        break;
+}
+
+foreach ($datos as $row) {
+    # code...
+}
+
+while ($a<=10){
+    echo $a;
+    $a++;
+}
+
+do {
+    echo $a;
+    $a++;
+} while ($a<=10);
+
+for ($b = 0; $b <=10; $b++) {
+    echo "b" . $b;
+}
+
+echo Text\Format::upperText(' hola');
+echo upper (' sergio');
+echo lower (' seRgio   ');
+echo 'debo aprender a colocar cambios de linea en php';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '85d61cf9b2c295024e6e067b41f77b9b9e87f76b',
+    'name' => 'sagalarzal/text',
+  ),
+  'versions' => 
+  array (
+    'sagalarzal/text' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '85d61cf9b2c295024e6e067b41f77b9b9e87f76b',
+    ),
+  ),
+);
